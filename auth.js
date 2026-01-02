605d636f24d32a98723475f9a66469550c5b3d98
Manoj_cs <abcdefghi6303@gmail.com>
2026-01-02 21:37:23 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,9 @@
+function register() {
+  alert("Registered successfully (backend will be added)");
+  window.location.href = "login.html";
+}
+
+function login() {
+  alert("Login successful (backend will be added)");
+  window.location.href = "index.html";
+}
